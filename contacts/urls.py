4737b27e9b291df31ad21f2220ba1bf1a3014bd0
noamolat-fs <noamolat@fullscale.io>
2023-07-05 16:38:13 +0800

    Use class views and add pagination

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from contacts import views
+from . import views
 
 app_name = "contacts"
 urlpatterns = [
-    path(f"{app_name}/", views.contacts, name="list"),
-    path(f"{app_name}/<int:pk>/", views.contact, name="view")
+    path(f"{app_name}/", views.Contacts.as_view(), name="list"),
+    path(f"{app_name}/<int:pk>/", views.Contact.as_view(), name="view")
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
--- contacts/views.py
+++ contacts/views.py
@@ -4,38 +4,14 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from contacts.models import Contact
 from contacts.serializers import ContactSerializer
 
-@api_view(["GET", "POST"])
-def contacts(request: HttpRequest, format: str=None):
-    if request.method == "GET":
-        contacts = Contact.objects.all()
-        serializer = ContactSerializer(contacts, many=True)
-        return Response(serializer.data)
-    elif request.method == "POST":
-        serializer = ContactSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-@api_view(["GET", "PUT", "DELETE"])
-def contact(request: HttpRequest, pk: int, format: str=None):
-    try:
-        contact = Contact.objects.get(pk=pk)
-    except Contact.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    if request.method == "GET":
-        serializer = ContactSerializer(contact)
-        return Response(serializer.data)
-    elif request.method == "PUT":
-        serializer = ContactSerializer(contact, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == "DELETE":
-        contact.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class Contacts(ListCreateAPIView):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+class Contact(RetrieveUpdateDestroyAPIView):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
