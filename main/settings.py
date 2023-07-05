b101107f8c0a11f68138250cfe831fc05dcca30c
noamolat-fs <noamolat@fullscale.io>
2023-07-05 23:22:30 +0800

    Add CORS headers

--- main/settings.py
+++ main/settings.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
-    "contacts"
+    "corsheaders",
+    "contacts",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -128,3 +130,5 @@ REST_FRAMEWORK = dict(
     DEFAULT_PAGINATION_CLASS="rest_framework.pagination.PageNumberPagination",
     PAGE_SIZE=10,
 )
+
+CORS_ALLOW_ALL_ORIGINS = True
