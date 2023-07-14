b25b8df59ed312b440cdc7d6b88c1a3334c203b5
noamolat-fs <noamolat@fullscale.io>
2023-07-14 17:51:56 +0800

    Change mobile limit to 12

--- contacts/models.py
+++ contacts/models.py
@@ -4,7 +4,7 @@ class Contact(models.Model):
     name = models.CharField(max_length=250)
     address = models.CharField(max_length=500, blank=True, default="")
     email = models.EmailField(max_length=250, blank=True, default="")
-    mobile = models.CharField(max_length=11, blank=True, default="")
+    mobile = models.CharField(max_length=12, blank=True, default="")
 
     class Meta:
         ordering = ["name"]
